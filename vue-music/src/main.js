2c8b9c16c7dd1bc1864fa09a9380cf30faab01eb
WangYuan <565971307@qq.com>
2018-09-11 20:55:02 +0800

    头部图片的导入

--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -1,8 +1,12 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastclick from 'fastclick'
 
 import 'common/stylus/index.styl'
+
+fastclick.attach(document.body)//清除DIV中搜索按钮的300毫秒的延迟
 
 // Vue.config.productionTip = false
 
