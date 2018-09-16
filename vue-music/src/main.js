e583603bd90a30ba849e9fa5e5a2e9993f24fee2
WangYuan <565971307@qq.com>
2018-09-16 19:24:34 +0800

    推荐页轮播图，歌手列表

--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -3,12 +3,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLazyload from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
 
 fastclick.attach(document.body)//清除DIV中搜索按钮的300毫秒的延迟
 
 // Vue.config.productionTip = false
+
+Vue.use(VueLazyload, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
